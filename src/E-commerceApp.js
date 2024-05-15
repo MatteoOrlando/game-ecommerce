09deb7770e5cd9dab1fe2bd2a78fcd2f9a4d4bb5
MatteoOrlando <mat.orlando88@gmail.com>
2024-05-15 15:28:07 +0200

    Update E-commerceApp.js

--- src/E-commerceApp.js
+++ src/E-commerceApp.js
@@ -1,12 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p></p>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/" component={ProductList} />
+        <Route path="/product/:id" component={ProductDetails} />
+        <Route path="/cart" component={Cart} />
+      </Switch>
+    </Router>
   );
 }
 
