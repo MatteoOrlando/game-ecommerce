2ade9bc56cde6d01848285dd012a311987bb3abd
MatteoOrlando <mat.orlando88@gmail.com>
2024-05-15 16:24:46 +0200

    Update Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">GameStore</Link>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/products">Prodotti</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/cart">Carrello</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
