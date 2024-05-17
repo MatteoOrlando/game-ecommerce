e762168d9195c47b3ac0f64b14f0dcafb7d48d8f
MatteoOrlando <mat.orlando88@gmail.com>
2024-05-17 12:51:19 +0200

    add pic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ import '../styles/Navbar.css';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark">
+        <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">
-                    <img src="/public/assets/Logo-3.webp" alt="" />
+                    <img src="/public/assets/Logo_2.webp" alt="" />
 
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
